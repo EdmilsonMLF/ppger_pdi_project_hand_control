9d9ab22e4ed980f6f9d22593a28fb82e945d7889
Edmilson Moreira <edmilson.mlf@gmail.com>
2024-05-23 12:10:32 -0300

    comentarios antes de encerrar parte 1

--- ControleVolumeMaos.py
+++ ControleVolumeMaos.py
@@ -15,6 +15,7 @@ captura = cv2.VideoCapture(0)
 captura.set(3, wCam) # 3 = CAP_PROP_FRAME_WIDTH
 captura.set(4, hCam) # 4 = CAP_PROP_FRAME_HEIGHT
 
+# operação do programa
 while True:
     success, img = captura.read() # teste de sucesso de captura
     
@@ -23,6 +24,7 @@ while True:
     fps = 1/(tempo_captura - tempo_anterior)
     tempo_anterior = tempo_captura
     
+    # apresenta FPS npo canto superior esquerdo
     cv2.putText(img, f'FPS: {int(fps)}', (40, 50),cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 3)
     cv2.imshow('Img', img) 
     cv2.waitKey(1) # 1ms de delay
