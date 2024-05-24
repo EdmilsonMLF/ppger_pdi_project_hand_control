48773f0465f35c42662b266dc3f9404518a387bc
Edmilson Moreira <edmilson.mlf@gmail.com>
2024-05-24 16:08:03 -0300

    identando o codigo funcional

--- ControleVolumeMaos.py
+++ ControleVolumeMaos.py
@@ -4,9 +4,8 @@ import time
 import numpy as np
 import HandTrackingModule as htm
 import math
-# controle de audio com https://github.com/AndreMiras/pycaw
 from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume # controle de audio com https://github.com/AndreMiras/pycaw
 
 # parametros da biblioteca de audio
 devices = AudioUtilities.GetSpeakers()
@@ -26,14 +25,11 @@ ajuste_volume_percentual = 0
 VOLUME_RANGE = volume.GetVolumeRange()
 VOLUME_MIN = VOLUME_RANGE[0]
 VOLUME_MAX = VOLUME_RANGE[1]
-# volume.SetMasterVolumeLevel(-20.0, None)
 
 # checar se camera funciona, em caso de erro colocar id = 1; 0 = open default camera
 captura = cv2.VideoCapture(0) 
 captura.set(3, wCam) # 3 = CAP_PROP_FRAME_WIDTH
 captura.set(4, hCam) # 4 = CAP_PROP_FRAME_HEIGHT
-
-
 detector = htm.handDetector()
 
 # operação do programa
@@ -44,16 +40,13 @@ while True:
     img = detector.findHands(img)
     # detectar a posição da mão
     lmList = detector.findPosition(img, draw=False)[0] # primeiro elemento tem os pontos da mão
+    
     if len(lmList) != 0:
-        #print(lmList[4], lmList[8])
-        
         # capturando as cordenadas x, y da ponta do polegar[4] e do indicador[8]
         # manual: https://mediapipe.readthedocs.io/en/latest/solutions/hands.html
-        
         x1, y1 = lmList[4][1], lmList[4][2]
         x2, y2 = lmList[8][1], lmList[8][2]
         cx, cy = (x1 + x2) // 2, (y1 + y2) // 2 # ponto médio da distancia entre a ponta dos dedos polegar e indicador
-        
         
         #criando circulos na ponta dos dedos
         cv2.circle(img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
@@ -64,7 +57,6 @@ while True:
         cv2.circle(img, (cx, cy), 5, (0, 0, 255), cv2.FILLED)
         
         tamanho_linha = math.hypot(x2 - x1, y2 - y1) # tamanho da linha varia com a distância da mão para a câmera além da distancia entre os dedos
-        #print(tamanho_linha)
         
         # criando efeito botão 
         if tamanho_linha < 40:
@@ -77,21 +69,15 @@ while True:
         ajuste_volume_tela = np.interp(tamanho_linha, [40, 250], [400, 150]) # novo range para apresentação em tela
         ajuste_volume_percentual = np.interp(tamanho_linha, [40, 250], [0, 100]) # novo range para apresentação em tela
         
-        print(tamanho_linha, ajuste_volume)
-        
         # ajustar o volume do sistema com os dedos
         volume.SetMasterVolumeLevel(ajuste_volume, None)
               
-     
     # colocar o volume do sistema na imagem
     cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3 ) 
     cv2.rectangle(img, (50, int(ajuste_volume_tela)), (85, 400), (255, 0, 0), cv2.FILLED )  
     
     # apresenta % do volume na tela
     cv2.putText(img, f'Volume: {int(ajuste_volume_percentual)} %', (40, 450),cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 3)
-        
-        
-        
         
     # ajuste dop FrameRate
     tempo_captura = time.time()
