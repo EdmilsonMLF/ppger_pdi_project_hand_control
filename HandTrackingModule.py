ebb84712d397a79779b17b9a332e92508476a708
Edmilson Moreira <edmilson.mlf@gmail.com>
2024-05-27 11:08:08 -0300

    identacao e PEP

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -18,7 +18,12 @@ class handDetector():
         self.trackCon = trackCon
         
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+        self.hands = self.mpHands.Hands(
+            self.mode, 
+            self.maxHands, 
+            self.detectionCon, 
+            self.trackCon
+        )
         self.mpDraw = mp.solutions.drawing_utils
         self.tipIds = [4, 8, 12, 16, 20]
 
@@ -30,7 +35,11 @@ class handDetector():
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
-                    self.mpDraw.draw_landmarks(img, handLms,self.mpHands.HAND_CONNECTIONS)
+                    self.mpDraw.draw_landmarks(
+                        img, 
+                        handLms,
+                        self.mpHands.HAND_CONNECTIONS
+                    )
         return img
 
     def findPosition(self, img, handNo=0, draw=True):
@@ -49,14 +58,27 @@ class handDetector():
                 # print(id, cx, cy)
                 self.lmList.append([id, cx, cy])
                 if draw:
-                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
-            xmin, xmax = min(xList), max(xList)
-            ymin, ymax = min(yList), max(yList)
+                    cv2.circle(
+                        img, 
+                        (cx, cy), 
+                        5, 
+                        (255, 0, 255), 
+                        cv2.FILLED
+                    )
+            xmin = min(xList)
+            xmax = max(xList)
+            ymin = min(yList)
+            ymax = max(yList)
             bbox = xmin, ymin, xmax, ymax
 
         if draw:
-            cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
-        (bbox[2] + 20, bbox[3] + 20), (0, 255, 0), 2)
+            cv2.rectangle(
+                img, 
+                (bbox[0] - 20, bbox[1] - 20),
+                (bbox[2] + 20, bbox[3] + 20), 
+                (0, 255, 0), 
+                2
+            )
         
         return self.lmList, bbox
 
@@ -77,17 +99,44 @@ class handDetector():
 
     def findDistance(self, p1, p2, img, draw=True):
 
-        x1, y1 = self.lmList[p1][1], self.lmList[p1][2]
-        x2, y2 = self.lmList[p2][1], self.lmList[p2][2]
-        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        x1 = self.lmList[p1][1]
+        y1 = self.lmList[p1][2]
+        x2 = self.lmList[p2][1]
+        y2 = self.lmList[p2][2]
+        cx = (x1 + x2) // 2
+        cy = (y1 + y2) // 2
         
         if draw:
-            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
-            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
-        
+            cv2.circle(
+                img, 
+                (x1, y1), 
+                15, 
+                (255, 0, 255), 
+                cv2.FILLED
+            )
+            cv2.circle(
+                img, 
+                (x2, y2), 
+                15, 
+                (255, 0, 255), 
+                cv2.FILLED
+            )
+            cv2.line(
+                img, 
+                (x1, y1), 
+                (x2, y2), 
+                (255, 0, 255), 
+                3
+            )
+            cv2.circle(
+                img, 
+                (cx, cy), 
+                15, 
+                (255, 0, 255), 
+                cv2.FILLED
+            )
         length = math.hypot(x2 - x1, y2 - y1)
+        
         return length, img, [x1, y1, x2, y2, cx, cy]
 
 def main():
@@ -105,8 +154,15 @@ def main():
     fps = 1 / (cTime - pTime)
     pTime = cTime
     
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
-    (255, 0, 255), 3)
+    cv2.putText(
+        img, 
+        str(int(fps)), 
+        (10, 70), 
+        cv2.FONT_HERSHEY_PLAIN, 
+        3,
+        (255, 0, 255), 
+        3
+    )
     
     cv2.imshow("Image", img)
     cv2.waitKey(1)
